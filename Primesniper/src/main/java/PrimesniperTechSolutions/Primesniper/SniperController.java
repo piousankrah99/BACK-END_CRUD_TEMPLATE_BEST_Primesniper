01c6afa647bce8077f0cb1369cbdd637841f4eb0
piousankrah99 <133825879+piousankrah99@users.noreply.github.com>
2024-03-10 14:38:37 +0000

    Update SniperController.java

--- Primesniper/src/main/java/PrimesniperTechSolutions/Primesniper/SniperController.java
+++ Primesniper/src/main/java/PrimesniperTechSolutions/Primesniper/SniperController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@RestController
+@Controller
 @RequestMapping(path="api/v1/Sniper")
 @AllArgsConstructor
 public class SniperController {
